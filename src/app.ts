29156676daa5adfcd143c7d3b311e63f1483d40b
ikexing-cn <ikexing@qq.com>
2022-10-25 21:54:46 +0800

    feat: use express to proxy http

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
-export const one = 1
-export const two = 2
+import { info } from 'console'
+import express from 'express'
 
-console.log(one, two)
+const PORT = 5701
+const app = express()
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  const { body, query } = req
+  console.log({ body, query })
+  res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+  info(`Server is running on port ${PORT}`)
+})
